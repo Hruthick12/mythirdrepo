acce229320bfa9182157103029efb05e61367d5f
hruthik <hruthik@gmail.com>
2023-05-22 18:20:34 +0530

    changes

--- shellscripts/deploy_java.sh
+++ shellscripts/deploy_java.sh
@@ -7,8 +7,8 @@ IP=`grep $env env.properties | cut -d ':' -f2 |tr -d "[:space:]"`
 mkdir /var/lib/jenkins/practice
 cd /var/lib/jenkins/practice
 aws s3 cp s3://jenkins-tests-8/java_build/$tag/sparkjava-hello-world-1.0.war .
-scp sparkjava-hello-world-1.0.war ec2-user@{IP}:/home/ec2-user/tomcat/apache-tomcat-9.0.74/webapps
-ssh ec2-user@{IP} << HERE
+scp sparkjava-hello-world-1.0.war ec2-user@${IP}:/home/ec2-user/tomcat/apache-tomcat-9.0.74/webapps
+ssh ec2-user@${IP} << HERE
     rm -rf /home/ec2-user/tomcat/apache-tomcat-9.0.64/work
     cd /home/ec2-user/tomcat/apache-tomcat-9.0.74/bin/
     ./shutdown.sh
