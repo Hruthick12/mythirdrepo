fbe30646f9fa2855515acdd6a78486d2d43cc936
hruthik <hruthik@gmail.com>
2023-05-22 18:14:18 +0530

    changes

--- shellscripts/deploy_java.sh
+++ shellscripts/deploy_java.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
+tag=$1
+env=$2
+
+IP=`grep $env env.properties | cut -d ':' -f2 |tr -d "[:space:]"`
+
 mkdir /var/lib/jenkins/practice
 cd /var/lib/jenkins/practice
-aws s3 cp s3://jenkins-tests-8/java_build/my-app-1.0-SNAPSHOT.jar .
-scp my-app-1.0-SNAPSHOT.jar ec2-user@10.0.0.171:/home/ec2-user/tomcat/apache-tomcat-9.0.74/webapps
-ssh ec2-user@10.0.0.171 << HERE
+aws s3 cp s3://jenkins-tests-8/java_build/$tag/sparkjava-hello-world-1.0.war .
+scp sparkjava-hello-world-1.0.war ec2-user@{IP}:/home/ec2-user/tomcat/apache-tomcat-9.0.74/webapps
+ssh ec2-user@{IP} << HERE
+    rm -rf /home/ec2-user/tomcat/apache-tomcat-9.0.64/work
     cd /home/ec2-user/tomcat/apache-tomcat-9.0.74/bin/
     ./shutdown.sh
     ./startup.sh
--- vars/deploy_tomcat.groovy
+++ vars/deploy_tomcat.groovy
@@ -1,4 +1,4 @@
-def deploy_tomcat() {
+def deploy_tomcat(String tag, String env) {
 
     echo "deploying to tomcat server."
     //echo "env is ${env}"
@@ -6,7 +6,7 @@ def deploy_tomcat() {
        #echo "WORKSPACE is "
        #echo "environment is $env'"
        cd ${WORKSPACE}/shellscripts
-       sh ./deploy_java.sh 
+       sh ./deploy_java.sh ${tag} ${env}
        """
 }
 return this
